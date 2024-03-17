8d03ec6294060c247d892dae6844890ed21b8cba
Gorthdzx <krislaksana123@gmail.com>
2024-03-18 05:46:54 +0700

    refactor : wrap intent into a composable function

--- app/src/main/java/app/dito/intentactivity/MainActivity.kt
+++ app/src/main/java/app/dito/intentactivity/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -17,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import app.dito.intentactivity.activity.SecondActivity
 import app.dito.intentactivity.ui.theme.IntentActivityTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,65 +29,80 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        /*
-                        Redirect to 2nd activity at TODO()youtube.com
-                        */
-                        Row {
-                            Button(onClick = {
-                                Intent(Intent.ACTION_MAIN).also {
-                                    it.`package` = "com.google.android.youtube"
-                                    try {
-                                        startActivity(it)
-                                    } catch (e: ActivityNotFoundException) {
-                                        e.printStackTrace()
-                                    }
-                                }
-                            }
-                            ) {
-                                Text(
-                                    text = "Go to youtube"
-                                )
-                            }
-                            /*
-                            Redirect to 2nd activity at TODO()SecondActivity.kt
-                             */
-                            Button(onClick = {
-                                Intent(applicationContext, SecondActivity::class.java).also {
-                                    startActivity(it)
-                                }
-                            }
-                            ) {
-                                Text(
-                                    text = "Go to 2nd activity"
-                                )
-                            }
-                        }
-
-                    }
+                    RedirectButton(appComponent = this)
                 }
             }
         }
     }
 }
 
-//SAMPLE
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun IntentAppPreview() {
     IntentActivityTheme {
-        Greeting("Android")
+
+    }
+}
+
+@Composable
+fun RedirectButton(appComponent: ComponentActivity) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        /*
+        Redirect to 2nd activity at
+        */
+
+        //Implicit Intent Implementation
+        Column {
+            Button(onClick = {
+                Intent(Intent.ACTION_MAIN).also {
+                    it.`package` = "com.google.android.youtube"
+                    try {
+                        appComponent.startActivity(it)
+                    } catch (e: ActivityNotFoundException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+            ) {
+                Text(
+                    text = "Go to youtube"
+                )
+            }
+            /*
+             //Explicit Intent implementation
+            Redirect to 2nd activity at
+            2nd Activity
+             */
+            Button(onClick = {
+                Intent(appComponent.applicationContext, SecondActivity::class.java).also {
+                    appComponent.startActivity(it)
+                }
+            }
+            ) {
+                Text(
+                    text = "Go to 2nd activity"
+                )
+            }
+
+            Button(onClick = {
+                Intent(Intent.ACTION_MAIN).also {
+                    it.`package` = "com.google.android.apps.maps"
+                    try {
+                        appComponent.startActivity(it)
+                    } catch (e: ActivityNotFoundException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+            ) {
+                Text(
+                    text = "Go to maps"
+                )
+            }
+        }
     }
 }
