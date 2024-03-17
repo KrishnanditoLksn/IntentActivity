4220922cd4bfc4e8a4e5cd0094ec8b4cb2f21f78
Gorthdzx <krislaksana123@gmail.com>
2024-03-18 05:48:22 +0700

    feat : added camera Intent

--- app/src/main/java/app/dito/intentactivity/activity/SecondActivity.kt
+++ app/src/main/java/app/dito/intentactivity/activity/SecondActivity.kt
@@ -1,16 +1,19 @@
-package app.dito.intentactivity
+package app.dito.intentactivity.activity
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import app.dito.intentactivity.MainActivity
 import app.dito.intentactivity.ui.theme.IntentActivityTheme
 
 class SecondActivity : ComponentActivity() {
@@ -21,10 +24,10 @@ class SecondActivity : ComponentActivity() {
                 Text(
                     text = "Hey it's 2nd activity"
                 )
-                Row(
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
                 ) {
                     Button(
                         onClick = {
@@ -39,6 +42,21 @@ class SecondActivity : ComponentActivity() {
                         Text(
                             text = "Back to main"
                         )
+                    }
+
+                    Button(onClick = {
+                        /*
+                        take a picture
+                         */
+                        val imageCapture = 1
+                        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                        try {
+                            startActivityForResult(takePictureIntent, imageCapture)
+                        } catch (e: ActivityNotFoundException) {
+                            // display error state to the user
+                        }
+                    }) {
+                        Text(text = "Take a Picture")
                     }
                 }
             }
