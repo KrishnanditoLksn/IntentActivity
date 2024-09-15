d40fb17f1f3c428063280592d7a052327f0a1ace
Gorthdzx <krislaksana123@gmail.com>
2024-09-15 16:42:31 +0700

    feat : parcelable activities and non

--- Explicit/app/src/main/java/app/ditodev/pickme/MainActivity.kt
+++ Explicit/app/src/main/java/app/ditodev/pickme/MainActivity.kt
@@ -1,19 +1,41 @@
 package app.ditodev.pickme
 
+import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import app.ditodev.pickme.activity.DataMoveActivity
 import app.ditodev.pickme.activity.FirstActivity
+import app.ditodev.pickme.activity.MoveResultActivity
+import app.ditodev.pickme.activity.ParcelableActivity
+import app.ditodev.pickme.data.model.Person
+import app.ditodev.pickme.data.`object`.Utils
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btn1: Button
     private lateinit var btn2: Button
+    private lateinit var groupResult: TextView
+
+    @SuppressLint("SetTextI18n")
+    private val registerResultActivits =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+        { result ->
+            if (result.resultCode == Utils.RESULT_CODE && result.data != null) {
+                val selectedVal = result.data?.getStringExtra(Utils.EXTRA_SELECTED_VALUE)
+                groupResult.text = "Your Favorite Player : $selectedVal"
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,9 +46,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             insets
         }
         btn1 = findViewById(R.id.button_id_1)
-        btn2 = findViewById(R.id.button_id_2)
+        btn1.setOnClickListener(this)
 
+        btn2 = findViewById(R.id.button_id_2)
         btn2.setOnClickListener(this)
+
+        val btnMoveObj: Button = findViewById(R.id.btn_move_activity_object)
+        btnMoveObj.setOnClickListener(this)
+
+        val btnImplicitIntent: Button = findViewById(R.id.btn_camera)
+        btnImplicitIntent.setOnClickListener(this)
+
+
+        val btnMoveRes: Button = findViewById(R.id.btn_move_for_result)
+        btnMoveRes.setOnClickListener(this)
+        groupResult = findViewById(R.id.intent_result_text)
     }
 
     override fun onClick(p0: View?) {
@@ -38,9 +72,36 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
             R.id.button_id_2 -> {
                 val dataMoveAct = Intent(this@MainActivity, DataMoveActivity::class.java)
-                dataMoveAct.putExtra(DataMoveActivity.EXTRA_NAME, "Lionel Messi")
-                dataMoveAct.putExtra(DataMoveActivity.EXTRA_AGE, "35")
+                dataMoveAct.putExtra(Utils.EXTRA_NAME, "Lionel Messi")
+                dataMoveAct.putExtra(Utils.EXTRA_AGE, "35")
                 startActivity(dataMoveAct)
+            }
+
+            R.id.btn_move_activity_object -> {
+                val person = Person(
+                    "Lionel Messi",
+                    35,
+                    "lioneloke@gmail.com",
+                    "Barcelona"
+                )
+
+                val moveObj = Intent(this@MainActivity, ParcelableActivity::class.java)
+                moveObj.putExtra(Utils.EXTRA_PERSON, person)
+                startActivity(moveObj)
+            }
+
+            R.id.btn_camera -> {
+                val camera = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                try {
+                    startActivity(camera)
+                } catch (e: ActivityNotFoundException) {
+                    error("Activity Not Found")
+                }
+            }
+
+            R.id.btn_move_for_result -> {
+                val moveRes = Intent(this@MainActivity, MoveResultActivity::class.java)
+                registerResultActivits.launch(moveRes)
             }
         }
     }
