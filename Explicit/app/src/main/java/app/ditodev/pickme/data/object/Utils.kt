7d64f5a4d04627786394d3e503b9d3de548536a3
Gorthdzx <krislaksana123@gmail.com>
2024-09-15 16:21:26 +0700

    feat:  models

---
+++ Explicit/app/src/main/java/app/ditodev/pickme/data/object/Utils.kt
@@ -0,0 +1,9 @@
+package app.ditodev.pickme.data.`object`
+
+object Utils {
+    const val EXTRA_PERSON = "extra_person"
+    const val EXTRA_AGE = "extra_age"
+    const val EXTRA_NAME = "extra_name"
+    const val EXTRA_SELECTED_VALUE = "extra_selected_value"
+    const val RESULT_CODE = 110
+}
